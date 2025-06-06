fe4bbc5ae37f0cb5338e64863bc859bc6bb4fd4e
Pionicle <stolyarovwork87@gmail.com>
2025-06-06 19:28:54 +0300

    sql scripts combined

--- sql/fill_data.py
+++ sql/fill_data.py
@@ -41,6 +41,68 @@ tables = [
     },
 ]
 
+create_script = """
+CREATE TYPE NATIONALITY AS ENUM ('Russian', 'American', 'British', 'French', 'German');
+
+CREATE TABLE authors (
+    author_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    nationality NATIONALITY NOT NULL
+);
+
+CREATE TYPE BOOK_CATEGORY AS ENUM ('Fiction', 'Non-fiction', 'Science', 'History', 'Fantasy');
+
+CREATE TABLE books (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(50) UNIQUE NOT NULL,
+    publication_year DATE NOT NULL,
+    category BOOK_CATEGORY NOT NULL,
+    author_id INT,
+    CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors (author_id) ON DELETE SET NULL
+);
+
+CREATE TABLE readers (
+    reader_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    CONSTRAINT valid_email CHECK (
+        email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
+    )
+);
+
+CREATE TABLE book_readers (
+    book_id INT,
+    reader_id INT,
+    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES books (book_id) ON DELETE CASCADE,
+    CONSTRAINT fk_reader FOREIGN KEY (reader_id) REFERENCES readers (reader_id) ON DELETE CASCADE,
+    PRIMARY KEY (book_id, reader_id)
+);
+"""
+
+synchronize_script = """
+SELECT setval(
+        'authors_author_id_seq', (
+            SELECT COALESCE(MAX(author_id), 0)
+            FROM authors
+        )
+    );
+
+SELECT setval(
+        'books_book_id_seq', (
+            SELECT COALESCE(MAX(book_id), 0)
+            FROM books
+        )
+    );
+
+SELECT setval('readers_reader_id_seq', (
+            SELECT COALESCE(MAX(reader_id), 0)
+            FROM readers
+        )
+    );
+"""
+
 
 def check_table_exists(cursor, table_name):
     """Проверка существования таблицы в базе данных."""
@@ -88,14 +150,15 @@ def main():
 
             # Проверка существования таблицы
             if not check_table_exists(cur, table_name):
-                logger.error(f"Table {table_name} does not exist in the database")
-                raise Exception(f"Table {table_name} does not exist")
+                cur.execute(create_script)
 
             logger.info(f"Importing data into {table_name} from {csv_file}...")
             import_csv_to_table(cur, table_name, csv_file, columns)
 
         # Подтверждение транзакции
         conn.commit()
+        # Синхронизация последовательности для таблиц
+        cur.execute(synchronize_script)
         logger.info("All data imported successfully")
 
     except (Exception, Error) as error:
